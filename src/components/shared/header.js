90e29907a1d9e5c0220be0cd55322777dbdd3103
Charlotte Davies <charlottemdavies@hotmail.co.uk>
2019-08-15 12:11:37 +0100

    awesome burger menu animation

--- src/components/shared/header.js
+++ src/components/shared/header.js
@@ -2,10 +2,36 @@ import React from "react";
 import "../../css/header.css";
 
 function Header() {
+  function handleBurger() {
+    const nav = document.querySelector(".burger-nav");
+    nav.classList.toggle("isopen");
+  }
+
   return (
-    <header>
-      <p>HEADER</p>
-      <p>Hello</p>
+    <header className="navbar">
+      <h1>Charlotte</h1>
+      <ul className="nav-menu">
+        <li>
+          <a href="/" alt="intro">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/intro" alt="intro">
+            Intro
+          </a>
+        </li>
+        <li>
+          <a href="/about" alt="about">
+            About
+          </a>
+        </li>
+      </ul>
+      <button className="burger-nav" id="burger" onClick={handleBurger}>
+        <span />
+        <span />
+        <span />
+      </button>
     </header>
   );
 }
