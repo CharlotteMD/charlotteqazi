a1c63369cf2d9dfe8df52eb017b4eb23ec0a808d
Charlotte Davies <charlottemdavies@hotmail.co.uk>
2019-08-19 11:43:29 +0100

    added first project

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,7 +2,43 @@ import React from "react";
 import "../css/style.css";
 
 function Projects() {
-  return <h1>Projects</h1>;
+  const items = [
+    {
+      title: "Whodunit",
+      image: "",
+      link: "https://github.com/CharlotteMD/wdi-first-project",
+      info:
+        "Whodunit remains one of my proudest project to date! I built it after just 2 weeks of learning JavaScript at General Assembly. I was so proud by how much I could achieve having learnt only a small amount of code."
+    },
+    {
+      title: "Whodunit",
+      image: "",
+      link: "https://github.com/CharlotteMD/wdi-first-project",
+      info:
+        "Whodunit remains one of my proudest project to date! I built it after just 2 weeks of learning JavaScript at General Assembly. I was so proud by how much I could achieve having learnt only a small amount of code."
+    }
+  ];
+
+  const MyProjects = ({ item }) => (
+    <li className="blockItem">
+      <img src={item.image} alt={item.title} />
+      <h3>{item.title}</h3>
+      <a href={item.link} alt={item.title} className="sideNote">
+        See project
+      </a>
+      <p>{item.info}</p>
+    </li>
+  );
+  return (
+    <div className="Projects">
+      <h1>Projects</h1>
+      <ul className="blockContainer">
+        {items.map(item => (
+          <MyProjects item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Projects;
