1a765cfeeed8a2d6e6a058d988768a9bff16d4dd
CharlotteMD <charlottemdavies@hotmail.co.uk>
2020-06-27 13:49:18 +0100

    Added extra projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -7,9 +7,14 @@ import guestbook from "../images/guestbook.png";
 import getaroom from "../images/getaroom.png";
 import appres from "../images/appres.png";
 import alexa from "../images/alexa.png";
+import sun from "../images/sun.png";
+import amplify from "../images/amplify.png";
 import portfolio from "../images/portfolio.jpg";
 import threejs from "../images/threejs.png";
 import advent from "../images/advent.png";
+import jaimy from "../images/jaimy.png";
+import axora from "../images/axora.png";
+import beema from "../images/beema.png";
 
 import "../css/style.css";
 
@@ -38,12 +43,22 @@ function Projects() {
     {
       title: "Alexa Skills",
       image: alexa,
-      link: "https://github.com/CharlotteMD/"
+      link: "https://github.com/CharlotteMD/alexa-skills"
+    },
+    {
+      title: "AWS Amplify",
+      image: amplify,
+      link: "https://github.com/CharlotteMD/Ultimate-To-Do-App"
     },
     {
       title: "Three.js",
       image: threejs,
-      link: "https://github.com/CharlotteMD/"
+      link: "https://github.com/CharlotteMD/3js"
+    },
+    {
+      title: "Holiday Packing App (WIP)",
+      image: sun,
+      link: "https://github.com/CharlotteMD/packing-list-app"
     },
     {
       title: "Advent Calendar",
@@ -57,26 +72,39 @@ function Projects() {
     }
   ];
 
+  const professionalProjects = [
+    {
+      title: "Beema",
+      image: beema,
+      link: "https://www.beema.ae/"
+    },
+    {
+      title: "Axora",
+      image: axora,
+      link: "https://www.axora.com/"
+    },
+    {
+      title: "Jaimy",
+      image: jaimy,
+      link: "https://jaimy.be/en/"
+    }
+  ];
+
   return (
     <div className="Projects">
       <h2 className="pageTitle">Projects</h2>
+      <h3 className="subtitle">Professional Projects</h3>
+      <ul className="blockContainer">
+        {professionalProjects.map(projects => (
+          <MyProjects project={projects} />
+        ))}
+      </ul>
+      <h3 className="subtitle">Side Projects</h3>
       <ul className="blockContainer">
         {projects.map(projects => (
           <MyProjects project={projects} />
         ))}
       </ul>
-      <p>
-        Please feel free to browse{" "}
-        <a href="https://github.com/CharlotteMD/" alt="GitHub">
-          my GitHub.
-        </a>
-      </p>
-      <p>
-        For examples of professional projects, please{" "}
-        <a href="mailto:charlottemarydavies@gmail.com" alt="GitHub">
-          get in touch.
-        </a>
-      </p>
     </div>
   );
 }
